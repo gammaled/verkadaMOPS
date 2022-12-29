ccbf489dea240e32f644d984c78b20dffe24aa1f
gammaled <guleduni@yahoo.co.uk>
2022-12-29 20:43:38 +0000

    Move get_response function above lambda_handler

--- lambda_handler.py
+++ lambda_handler.py
@@ -33,25 +33,6 @@ class VerkadaDB():
 ## Do not edit   
 dbInstance = VerkadaDB()  
 
-## To-do: Implement Function (mimics AWS Lambda handler)
-## Input: JSON String which mimics AWS Lambda input
-def lambda_handler(json_input):
-    global dbInstance
-    json_input = json.loads(json_input)
-    split_result = re.split(r"[@\.]", json_input["email"])
-    email = json_input["email"]
-    name = split_result[0]
-    domain = split_result[1]
-    topLevelName = split_result[-1]
-
-    print(get_response(name))
-
-
-    print(name)
-    print(domain)
-    print(topLevelName)
-    print(email)
-
 def get_response(name):
     agify_url = "https://api.agify.io?name=" + name
     genderize_url = "https://api.genderize.io?name=" + name
@@ -66,6 +47,24 @@ def get_response(name):
     country = country_response["country"][0]["country_id"]
 
     return [age, gender, country]
+
+## To-do: Implement Function (mimics AWS Lambda handler)
+## Input: JSON String which mimics AWS Lambda input
+def lambda_handler(json_input):
+    global dbInstance
+    json_input = json.loads(json_input)
+    split_result = re.split(r"[@\.]", json_input["email"])
+    email = json_input["email"]
+    name = split_result[0]
+    domain = split_result[1]
+    topLevelName = split_result[-1]
+
+    print(get_response(name))
+
+    print(name)
+    print(domain)
+    print(topLevelName)
+    print(email)
 
 
 
